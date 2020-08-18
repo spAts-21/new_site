376f84f91cfbc02b916cd221d5482bb34145184d
priyavarshoo <59841240+priyavarshoo@users.noreply.github.com>
2020-08-18 12:20:04 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -44,9 +44,9 @@ $(document).ready(function () {
             }    
 			
 			'<div style="border:thin solid black; background-color:white">' + value.Photo + value["Name"] + value["Post"] + value["Department"] + 
-			value["Email id"] + value["Fb link"] +  value["Linkedin link"]  + '</div>' ;
+			value["Email id"] + value["Fb link"] +  value["Linkedin link"] + '<br/>' + '</div>' ;
 				
-		
+			
 			
 			//'<div style="border:thin solid black; padding: 5px">' +  value["Name"] + value["Post"] + '</div>';
 			//')'
