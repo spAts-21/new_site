75fcb78c96128c60e7870966609c6dde7f2f44a0
priyavarshoo <59841240+priyavarshoo@users.noreply.github.com>
2020-08-22 20:52:40 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -47,8 +47,8 @@ $(document).ready(function () {
                 Id="#";
             }
 			document.getElementById("showExcel").innerHTML+='<div id="person">' + '<img class=pericon"; src=" https://drive.google.com/uc?id='+Id+'">' + '<br/>' + value["Name"] + 
-			'<br/>' + value["Post"] + '<br/>' + value["Department"] + '<br/>' + value["Email id"] + '<br/>' + '<a href="'+value["fb link"]+'">' + fb + '</a>' + '<br/>' + 
-			'<a href="' + value["linkedin link"] + '">' + Li + '</a>' + '</div>' + '<br/>' ;
+			'<br/>' + value["Post"] + '<br/>' + value["Department"] + '<br/>' + value["Email id"] + '<br/>' + '<a href="'+value["fb link"]+'">fb</a>' + '<br/>' + 
+			'<a href="' + value["linkedin link"] + '"> Li </a>' + '</div>' + '<br/>' ;
 			
         });
 		
