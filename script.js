49be14b23fdde24d7b7b9dc40bd652df29575e5f
priyavarshoo <59841240+priyavarshoo@users.noreply.github.com>
2020-08-26 19:41:13 +0530

    js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function(jQuery) {
     
     jQuery('#content').css('display', 'block');
 });
+
 jQuery('ul.navbar-nav li.dropdown').hover(function() {
     jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
   }, function() {
